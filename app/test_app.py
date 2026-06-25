0d25811fc922f8b20ffcfbe022a144b91417a4e8
Zeeshan Ahmad <zeeshankhattak002@gmail.com>
2026-06-25 11:49:31 +0000

    Added pytest and pylint foundation

---
+++ app/test_app.py
@@ -0,0 +1,11 @@
+from app import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_health():
+    client = app.test_client()
+    response = client.get("/health")
+    assert response.status_code == 200
